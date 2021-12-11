3a0ded0dfba31e02cb9173af731bf4d06dc43553
sadia <sadiakhan7545@gmail.com>
2021-12-11 15:16:48 -0500

    seconf

---
+++ main.py
@@ -0,0 +1,10 @@
+
+name = input("what is your name? ")
+print("hi " + name + "!")
+
+adjective = input("Type a adjective. ")
+verb = input("Now, type a verb. ")
+adjective2 = input("Now, type another adjective. ")
+noun = input("Now, type a noun. ")
+
+print ("Our school has really " + adjective + " food. Just thinking about it makes my stomach " + verb + ". The pizza is " + adjective2 + " and tastes like " + noun)
